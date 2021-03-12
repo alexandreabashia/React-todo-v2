7ad457286721f17f6fad9b9389c7e57b8b2c0d09
unknown <alexandreabashia@gmail.com>
2021-03-12 22:38:11 +0400

    initial commit

---
+++ src/components/Form.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default function form({ todos, setTodos, inputText, setInputText }) {
+
+    function inputTextHandler(e) {
+        setInputText(e.target.value);
+    }
+
+    function submitTodoHandler(e) {
+        e.preventDefault();
+        setTodos([
+            {
+                text: inputText, completed: false, id: Math.random() * 100
+            },
+            ...todos
+        ])
+        setInputText("");
+    }
+
+    return (
+        <div>
+            <form>
+                <input onChange={inputTextHandler} type="text" className="todo-input" />
+                <button onClick={submitTodoHandler} className="todo-button" type="submit">
+                    <i className="fas fa-plus-square"></i>
+                </button>
+                <div className="select">
+                    <select name="todos" className="filter-todo">
+                        <option value="all">All</option>
+                        <option value="completed">Completed</option>
+                        <option value="uncompleted">Uncompleted</option>
+                    </select>
+                </div>
+            </form>
+        </div>
+    )
+}
---
+++ src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function TodoList() {
+    return (
+        <div>
+            <div className="todo-container">
+                <ul className="todo-list"></ul>
+            </div>
+        </div>
+    )
+}
