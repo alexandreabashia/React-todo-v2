15115b0bd88855ddfa324b685b48c0ab7f373aca
unknown <alexandreabashia@gmail.com>
2021-03-13 20:35:01 +0400

    add option listener in Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -17,6 +17,10 @@ export default function form({ todos, setTodos, inputText, setInputText }) {
         setInputText("");
     }
 
+    function filterTodo(e){
+        console.log(console.log(e.target.value))
+    }
+
     return (
         <div>
             <form>
@@ -25,7 +29,7 @@ export default function form({ todos, setTodos, inputText, setInputText }) {
                     <i className="fas fa-plus-square"></i>
                 </button>
                 <div className="select">
-                    <select name="todos" className="filter-todo">
+                    <select name="todos" onChange={filterTodo} className="filter-todo">
                         <option value="all">All</option>
                         <option value="completed">Completed</option>
                         <option value="uncompleted">Uncompleted</option>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,8 +2,7 @@ import React from 'react';
 import Todo from './Todo';
 
 export default function TodoList({ todos, setTodos }) {
-
-
+    
     return (
         <div>
             <div className="todo-container">
